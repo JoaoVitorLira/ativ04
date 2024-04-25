671ddffd0632c7490d962d3572332bcfb85259dc
João Lira <joao.liraves07@gmail.com>
2024-04-25 15:13:24 -0300

    Initial commit

---
+++ Exercicio04.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+
+//1 – Peça para o usuário digitar um número de 1 a 7, e mostre
+//o dia da semana correspondente, por exemplo, se o usuário
+//digitar 1, mostre Domingo.Se o usuário digitar 2, mostre
+//Segunda - Feira, e assim por diante.Caso o usuário digite
+//algum outro número mostre dia invalido.
+
+using namespace std;
+
+int main() {
+	//	int numeros;
+	//	cout << "Digite um numero entre 1 e 7: ";
+	//	cin >> numeros;
+	//
+	//		switch (numeros) {
+	//		case 1:
+	//			cout << "Domingo";
+	//			break;
+	//
+	//		case 2:
+	//			cout << "Segunda-Feira";
+	//			break;
+	//
+	//		case 3:
+	//			cout << "Terça-Feira";
+	//			break;
+	//
+	//		case 4:
+	//			cout << "Quarta-feira";
+	//			break;
+	//
+	//		case 5:
+	//			cout << "Quinta-feira";
+	//			break;
+	//
+	//		case 6:
+	//			cout << "Sexta-feira";
+	//			break;
+	//
+	//		case 7:
+	//			cout << "Sabado";
+	//			break;
+	//			
+
+
+	//2 – Peça para o usuário informar dois números e a operação
+	//matemática que ele deseja(+, -, *ou / ), mostre o resultado
+	//da conta de acordo com a operação que ele escolheu
+
+	//float num1;
+	//float num2;
+	//char operacao;
+	//int resultado;
+	//
+	//int numero1;
+	//cout << "Insira um numero: ";
+	//cin >> numero1;
+	//
+	//int numero2;
+	//cout << "Insira outro numero: ";
+	//cin >> numero2;
+	//}
+	//cout << "Informe a operação matemática: (+ , - , * , /)";
+	//cin >> operacao;
+	//
+	//switch (operacao)
+	//	{
+	//	case '+':
+	//		resultado = num1 + num2;
+	//		cout << "O resultado é: " << resultado;
+	//		break;
+	//	case '-':
+	//		resultado = num1 - num2;
+	//		cout << "O resultado é: " << resultado;
+	//		break;
+	//	case '*':
+	//		resultado = num1 * num2;
+	//		cout << "O resultado é: " << resultado;
+	//		break;
+	//	case '/':
+	//		resultado = num1 / num2;
+	//		cout << "O resultado é: " << resultado;
+	//		break;
+	//	default:
+	//		break;
+	//	}
+	//	
+	//
+	//}
+
+
+	//3 – Peça para o usuário digitar uma letra, se a letra for A, E, I,
+	//	O ou U, mostre vogal, caso ele digite outra letra, mostre
+	//	consoante
+
+
+	//setlocale(LC_ALL, "pt_BR.UTF-8");
+	//
+	//	char letra;
+	//	cout << "Digite uma letra:";
+	//	cin >> letra;
+	//
+	//	switch (letra)
+	//	{
+	//	case 'a':
+	//	case 'A':
+	//		{
+	//		cout << "A letra " << letra << " é uma vogal!";
+	//		break;
+	//		}
+	//	case 'e':
+	//	case 'E':
+	//		{
+	//		cout << "A letra " << letra << " é uma vogal!";
+	//		break;
+	//		}
+	//	case 'i':
+	//	case 'I':
+	//		{
+	//		cout << "A letra " << letra << " é uma vogal!";
+	//		break;
+	//		}
+	//	case 'o':
+	//	case 'O':
+	//		{
+	//		cout << "A letra " << letra << " é uma vogal!";
+	//		break;
+	//		}
+	//	case 'u':
+	//	case 'U':
+	//		{
+	//		cout << "A letra " << letra << " é uma vogal!";
+	//		break;
+	//		}
+	// 
+	//	default:
+	//		cout << "A letra " << letra << " é uma consoante";
+	//		break;
+	//	}
+
+}
